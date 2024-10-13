08fa97d3aa04a99e44200648dda9aff954963c28
Kendal Minor <123659794+kembl-codeferret@users.noreply.github.com>
2024-10-13 00:08:40 -0500

    hahha i made a windor
    tired go to bed now :)

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+w = Tk(className="rock judging time !")
+
+# objects
+
+TS_Title = ttk.Label(text="Welcome, judge, to\nThe Official Rock Judging GUI!", padding=25, font=35, )
+
+# draw
+
+TS_Title.pack()
+
+w.mainloop()
