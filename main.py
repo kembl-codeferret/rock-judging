8924e0b5319b2c75ab7560bdcf70cfd1f359bb50
Kendal Minor <123659794+kembl-codeferret@users.noreply.github.com>
2024-10-14 11:26:19 -0500

    Finished (?) title screen

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from tkinter import *
 from tkinter import ttk
+import os, csv
 
 w = Tk(className="rock judging time !")
 
+def scene2():
+    pass
+
 # objects
 
-TS_Title = ttk.Label(text="Welcome, judge, to\nThe Official Rock Judging GUI!", padding=25, font=35, )
+TS_Title = ttk.Label(text="Welcome, judge, to\nThe Official Rock Judging GUI!", font=35)
+TS_Button = ttk.Button(text="Begin Judging", command=scene2)
 
 # draw
 
-TS_Title.pack()
+TS_Title.pack(padx=25,pady=12)
+TS_Button.pack(padx=25, pady=13)
 
 w.mainloop()
