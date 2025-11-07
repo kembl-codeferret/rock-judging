e4fa78760fd2f336bfde6c92a185c814d0452030
Kendal Minor <k.minor2205@gmail.com>
2025-11-07 17:17:57 -0600

    orient the images before resizing

--- image_downloader.py
+++ image_downloader.py
@@ -13,12 +13,12 @@ def prepare_image(_input):
             return "images/" + name
     image = gdown.download(_input, "images/" + name, fuzzy=True) # if it doesn't, download the file with the encrypted name
     with Image.open(image) as _img: # rescale the image and resave the rescaled image
-        ratio = 500 / _img.width
+        _img = ImageOps.exif_transpose(_img) # rotate image from exif tags
+        ratio = 375 / _img.width
         _img = _img.resize((int(_img.width * ratio), int(_img.height * ratio)))
         _img = _img.convert("RGB")
-        # with Qt, I don't need to do the rescaling because, unlike Tk, Qt will resize the image to fit the widget,
-        # but I'm keeping it for loading speed sake. rather waste time now than every time the image is loaded
-        _img = ImageOps.exif_transpose(_img) # rotate image from exif tags
+        # Qt does resize images to fit the window, however it does not keep aspect ratio.
+        # this will not fix the issue entirely but for most pictures it should be fine
 
         _img.save("images/" + name, format="jpeg", optimize=True, quality=35)
 
